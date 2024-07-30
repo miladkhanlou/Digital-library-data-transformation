93456933990d3489280a90b155fafff367be1d61
Milad Khanlou <104443908+miladkhanlou@users.noreply.github.com>
2024-07-30 15:44:05 -0500

    Update Data-Extract.sh

--- Data-Extract.sh
+++ Data-Extract.sh
@@ -6,19 +6,3 @@ drush -u 1 islandora_datastream_crud_fetch_pids --namespace=COLLECTION_NAME --pi
 drush -u 1 idcrudfd --pid_file=/tmp/COLLECTION_NAME.txt --datastreams_directory=/tmp/COLLECTION_NAME --dsid=MODS
 drush -u 1 idcrudfd --pid_file=/tmp/COLLECTION_NAME.txt --datastreams_directory=/tmp/COLLECTION_NAME --dsid=RELS-EXT
 drush -u 1 idcrudfd --pid_file=/tmp/COLLECTION_NAME.txt --datastreams_directory=/tmp/COLLECTION_NAME --dsid=OBJ
-
-#
-cd /tmp/COLLECTION_NAME
-zip COLLECTION_NAME.zip *
-
-ctl-D
-
-scp dgi-ingest:/tmp/COLLECTION_NAME/COLLECTION_NAME.zip ~/Downloads
-cp ~/Downloads/COLLECTION_NAME.zip /meda/wwc/0A2C-888E/COLLECTION_NAME/
-
-cd /media/wwc/0A2C-888E/amistad-pgoudvis
-mkdir Data
-cd Data
-unzip ../COLLECTION_NAME.zip 
-
-for f in *.jp2; do opj_decompress -i "$f" -OutFor PNG -o "${f%.*}.png"; done;
